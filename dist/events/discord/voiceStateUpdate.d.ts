97673580be96397988870386fd1ebb8b24293da9
kuzeyalpyildiz <kuzeyalpyildiz@gmail.com>
2026-07-26 16:16:15 +0300

    Delete dist directory

--- dist/events/discord/voiceStateUpdate.d.ts
+++
@@ -1,8 +0,0 @@
-import { VoiceState } from 'discord.js';
-import { ZorinClient } from '../../structures/ZorinClient';
-declare const _default: {
-    name: string;
-    once: boolean;
-    execute(client: ZorinClient, oldState: VoiceState, newState: VoiceState): Promise<void>;
-};
-export default _default;
